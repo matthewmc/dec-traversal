a2ad3ca80ef884428f1ff7c94e4e401597b358be
Matthew C <matthewchu@ucla.edu>
2015-02-25 07:02:48 -0800

    Introduces some tests, moves some things around.

---
+++ examples/dynamicgraph.py
@@ -0,0 +1,23 @@
+from ..main import inttrav
+
+a = Letter('a')
+b = Letter('b')
+c = Letter('c')
+d = Letter('d')
+e = Letter('e')
+f = Letter('f')
+g = Letter('g')
+h = Letter('h')
+a.children = [b, c, f]
+c.children = [d, e]
+f.children = [e, g]
+e.children = [h]
+g.children = [g, h]
+
+@dfs 
+def travel(Node):
+    print(Node.letter)
+    return True
+
+if __name__ == '__main__':
+    travel(a)
